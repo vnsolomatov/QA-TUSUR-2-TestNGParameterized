3cf48a2fdc440b68a96e93599fc1e7c39bd654ef
vnsolomatov <solomatov.mail@gmail.com>
2022-02-06 15:47:56 +0300

    separator character comma replaced with space

--- unit-online-tusur/src/test/java/online_tusur/unit_online_tusur/NewTestNGParam.java
+++ unit-online-tusur/src/test/java/online_tusur/unit_online_tusur/NewTestNGParam.java
@@ -7,15 +7,15 @@ public class NewTestNGParam {
 	  @DataProvider(name = "dataAges")
 	  public static Object[][] getAverAge() {
 		  return new Object[][] {
-			  {"18,19,20,21,22,23,24,25","21"}, 
-			  {"26,27,28,29,30,31,32,33,34,35,36","31"},
-			  {"37,38,39,40,41,42,43,44,45,46,47,48,49","43"},
+			  {"18 19 20 21 22 23 24 25","21"}, 
+			  {"26 27 28 29 30 31 32 33 34 35 36","31"},
+			  {"37 38 39 40 41 42 43 44 45 46 47 48 49","43"},
 		  };
 	  }
 		
 	  @Test(dataProvider = "dataAges")
 	  void testAverAge(String ages, String expectedAverAge) {
-		  String[] ageA = ages.split(",");
+		  String[] ageA = ages.split(" ");
 			Student[] sA = new Student[ageA.length];
 			for (int i = 0; i < ageA.length; i++) {
 				sA[i] = new Student();
@@ -27,7 +27,7 @@ public class NewTestNGParam {
   @Test
   @Parameters({"averAge","ages"})
   public void params(@Optional("0")int averAge, @Optional("null")String ages) {
-	  String[] ageA = ages.split(",");
+	  String[] ageA = ages.split(" ");
 		Student[] sA = new Student[ageA.length];
 		for (int i = 0; i < ageA.length; i++) {
 			sA[i] = new Student();
